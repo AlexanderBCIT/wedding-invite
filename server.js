c77b99346d9e202cd0e7ea47468b9d6209bc295e
AlexanderBCIT <xanderbgrahamit@gmail.com>
2026-06-24 00:18:37 +0200

    added correct url

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://alexander-and-chantane.netlify.app', 'http://localhost:3000']
+}));
 app.use(express.json());
 app.use(express.static('.'));
 
